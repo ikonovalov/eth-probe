a48595d80bf0ec7909023e7eec4b26b30d0d166e
ikonovalov <konovalov84@gmail.com>
2016-12-20 01:10:26 +0300

    Implements 'tx to' scanner

--- index.js
+++ index.js
@@ -5,6 +5,24 @@
 
 const txMod = require('./lib/tx');
 
+const composeBlockScanOptions = (eth, options) => {
+    let fromBlockNumber = options.fromBlock || 0;
+    let toBlockNumber = (!options.toBlock || options.toBlock === 'latest') ? eth.blockNumber : options.toBlock;
+
+    if (options.fromOffset && !options.fromBlock) {
+        fromBlockNumber = toBlockNumber - options.fromOffset;
+    }
+
+    return {
+        fromBlockNumber: fromBlockNumber,
+        toBlockNumber: toBlockNumber
+    }
+}
+
+const flatten = list => list.reduce(
+    (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
+);
+
 class EthProbe {
 
     constructor(web3) {
@@ -13,18 +31,60 @@ class EthProbe {
 
     get tx() {
         return {
-            fromAddress: (address, options = {}) => {
-                const eth = this._web3.eth;
-                let startBlockNumber = options.startBlock || 0;
-                let endBlockNumber = options.endBlock || eth.blockNumber;
+            /**
+             * Search TXs using log filtration algorithm.
+             * @param address
+             * @param options
+             * @returns {Promise}
+             */
+            toAddressFast: (address, options = {}) => {
+                let eth = this._web3.eth;
+                let composedOptions = composeBlockScanOptions(eth, options);
 
-                console.log(`Scan from ${startBlockNumber} to ${endBlockNumber}`);
-                for (let i = startBlockNumber; i <= endBlockNumber; i++) {
-                    let block = eth.getBlock(i, true);
-                    if (block.transactions.length) {
-                        console.log(block.number + ' -> ' + block.transactions.length)
+                let filter = eth.filter({
+                    address: address,
+                    fromBlock: composedOptions.fromBlockNumber,
+                    toBlock: composedOptions.toBlockNumber
+                });
+
+                return new Promise((resolve, rejected) => {
+                    filter.get((error, result) => {
+                        if (!error) {
+                            let txs = result.map(log => log.transactionHash);
+                            resolve(txs);
+                        } else {
+                            rejected(error)
+                        }
+                    });
+                });
+            },
+
+            /**
+             * Search TXs using full block scan algorithm.
+             * @param address
+             * @param options
+             * @returns {Promise}
+             */
+            toAddressFullScan: (address, options = {}) => {
+                let eth = this._web3.eth;
+                let composedOptions = composeBlockScanOptions(eth, options);
+                let promise = new Promise((resolved, rejected) => {
+                    try {
+                        let txs = [];
+                        for (let i = composedOptions.fromBlockNumber; i <= composedOptions.toBlockNumber; i++) {
+                            let block = eth.getBlock(i, true);
+                            if (block.transactions.length > 0) {
+                                let blockFiltredTxHashes = block.transactions.filter(tx => tx.to == address).map(tx => tx.hash);
+                                txs.push(blockFiltredTxHashes);
+                            }
+                        }
+                        resolved(flatten(txs))
+                    } catch (error) {
+                        rejected(error)
                     }
-                }
+                });
+                return promise
+
             }
         }
     }
