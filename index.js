3e06cc638d732b5f75341ca163b7ef285545fc8e
ikonovalov <konovalov84@gmail.com>
2016-12-23 01:16:15 +0300

    Rebuild for ES6

--- index.js
+++ index.js
@@ -38,92 +38,103 @@ const flatten = list => list.reduce(
     (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
 );
 
-class EthProbe {
+class TransactionProbe {
 
     constructor(web3) {
         this._web3 = web3;
     }
 
-    get tx() {
-        return {
-
-            /**
-             * Get original Web3 tx (REQUIRE Geth 1.5+)
-             * @param txHash
-             * @returns {*}
-             */
-            get: (txHash) => {
-                return this._web3.eth.getTransaction(txHash)
-            },
-
-            /**
-             * Get ethereumjs-Tx.
-             * @param txHash
-             */
-            getX: (txHash) => {
-                let gtx = this._web3.eth.getTransaction(txHash);
-                let tx = buildTx(gtx);
-                return tx;
-            },
-
-            /**
-             * Search TXs using log filtration algorithm.
-             * @param address
-             * @param options
-             * @returns {Promise}
-             */
-            toAddressFast: (address, options = {}) => {
-                let eth = this._web3.eth;
-                let composedOptions = composeBlockScanOptions(eth, options);
-
-                let filter = eth.filter({
-                    address: address,
-                    fromBlock: composedOptions.fromBlockNumber,
-                    toBlock: composedOptions.toBlockNumber
-                });
-
-                return new Promise((resolve, rejected) => {
-                    filter.get((error, result) => {
-                        if (!error) {
-                            let txs = result.map(log => log.transactionHash);
-                            resolve(txs);
-                        } else {
-                            rejected(error)
-                        }
-                    });
-                });
-            },
-
-            /**
-             * Search TXs using full block scan algorithm.
-             * @param address
-             * @param options
-             * @returns {Promise}
-             */
-            toAddressFullScan: (address, options = {}) => {
-                let eth = this._web3.eth;
-                let composedOptions = composeBlockScanOptions(eth, options);
-                let promise = new Promise((resolved, rejected) => {
-                    try {
-                        let txs = [];
-                        for (let i = composedOptions.fromBlockNumber; i <= composedOptions.toBlockNumber; i++) {
-                            let block = eth.getBlock(i, true);
-                            if (block.transactions.length > 0) {
-                                let blockFiltredTxHashes = block.transactions.filter(tx => tx.to == address).map(tx => tx.hash);
-                                txs.push(blockFiltredTxHashes);
-                            }
-                        }
-                        resolved(flatten(txs))
-                    } catch (error) {
-                        rejected(error)
-                    }
-                });
-                return promise
-
-            }
-        }
+    /**
+     * Get original Web3 tx (REQUIRE Geth 1.5+)
+     * @param txHash
+     * @returns {*}
+     */
+    get(txHash) {
+        return this._web3.eth.getTransaction(txHash)
     }
 
+    /**
+     * Get ethereumjs-Tx.
+     * @param txHash
+     */
+    getX(txHash) {
+        let gtx = this._web3.eth.getTransaction(txHash);
+        let tx = buildTx(gtx);
+        return tx;
+    }
+
+    /**
+     * Search TXs using log filtration algorithm.
+     * @param address
+     * @param options
+     * @returns {Promise}
+     */
+    toAddressFast(address, options = {}) {
+        let eth = this._web3.eth;
+        let composedOptions = composeBlockScanOptions(eth, options);
+
+        let filter = eth.filter({
+            address: address,
+            fromBlock: composedOptions.fromBlockNumber,
+            toBlock: composedOptions.toBlockNumber
+        });
+
+        return new Promise((resolve, rejected) => {
+            filter.get((error, result) => {
+                if (!error) {
+                    let txs = result.map(log => log.transactionHash);
+                    resolve(txs);
+                } else {
+                    rejected(error)
+                }
+            });
+        });
+    }
+
+    fromToFast(addressFrom, addressTo, options = {}) {
+        //let txHashes =
+    }
+
+    /**
+     * Search TXs using full block scan algorithm.
+     * @param address
+     * @param options
+     * @returns {Promise}
+     */
+    toAddressFullScan(address, options = {}) {
+        let eth = this._web3.eth;
+        let composedOptions = composeBlockScanOptions(eth, options);
+        let promise = new Promise((resolved, rejected) => {
+                try {
+                    let txs = [];
+                    for (let i = composedOptions.fromBlockNumber; i <= composedOptions.toBlockNumber; i++) {
+                        let block = eth.getBlock(i, true);
+                        if (block.transactions.length > 0) {
+                            let blockFiltredTxHashes = block.transactions.filter(tx => tx.to == address).map(tx => tx.hash);
+                            txs.push(blockFiltredTxHashes);
+                        }
+                    }
+                    resolved(flatten(txs))
+                } catch
+                    (error) {
+                    rejected(error)
+                }
+            })
+            ;
+        return promise
+    }
+}
+
+class EthProbe {
+
+    constructor(web3) {
+        this.transactionProbe = new TransactionProbe(web3);
+    }
+
+    get tx() {
+        return this.transactionProbe;
+
+    }
 }
 
 module.exports = EthProbe;
